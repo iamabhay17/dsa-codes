d01e762eca64d1279191a70894f2ce504182b908
Abhay kumar bhardwaj <2001abhaybhardwaj@gmail.com>
2023-02-02 20:16:06 +0530

    add: print reverse list

---
+++ Linked Lists/Singly Linked List/problem statements/printReverse.cpp
@@ -0,0 +1,74 @@
+// Print reverse of a linked list without actually reversing it
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int d)
+    {
+        this->next = NULL;
+        this->data = d;
+    }
+};
+
+//--------------------inserting data----------------------------------
+void push(Node *&tail, int d)
+{
+    Node *temp = new Node(d);
+
+    tail->next = temp;
+    tail = tail->next;
+}
+
+//-----------------------display---------------------------------------
+
+void display(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+//----------------------print reverse----------------------------
+
+void printReverse(Node *&head)
+{
+    Node *temp = head;
+    if (temp == NULL)
+        return;
+
+    printReverse(temp->next);
+    cout << temp->data << " ";
+}
+
+//-----------------------main-fn-----------------------------------------
+
+int main()
+{
+    Node *newNode = new Node(5);
+    Node *head = newNode, *tail = newNode;
+
+    // data to be used
+    push(tail, 6);
+    push(tail, 7);
+    push(tail, 8);
+    push(tail, 9);
+    push(tail, 10);
+    push(tail, 11);
+    display(head);
+
+    // reverse linked list
+
+    printReverse(head);
+
+    return 0;
+}
