f695982581b17f60391388751eed71b255f71f4d
Abhay kumar bhardwaj <2001abhaybhardwaj@gmail.com>
2023-02-06 19:00:21 +0530

    add : queue reverse

--- Queue/implement.cpp
+++ Queue/implement.cpp
@@ -1,6 +1,6 @@
 // implementing queue class in cpp
 
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 class Queue
@@ -56,6 +56,21 @@ public:
             cout << arr[i] << " ";
         }
     }
+    void reverse()
+    {
+        stack<int> s;
+
+        for (int i = front; i <= rear; i++)
+        {
+            s.push(arr[i]);
+            deque();
+        }
+        while (!s.empty())
+        {
+            enque(s.top());
+            s.pop();
+        }
+    }
 };
 
 int main()
@@ -63,12 +78,15 @@ int main()
     Queue one(5);
     one.deque();
     one.enque(1);
-    one.enque(1);
-    one.enque(1);
-    one.enque(1);
-    one.enque(1);
-    one.enque(1);
+    one.enque(2);
+    one.enque(3);
+    one.enque(4);
+    one.enque(5);
+    one.enque(6);
     one.deque();
     one.display();
+    one.reverse();
+    one.display();
+
     return 0;
 }
